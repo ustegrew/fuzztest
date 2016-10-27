4bf7a6fbc4561f7b40279901c278209cc2b0d4d1
peter <peter.hoppe@gmail.com>
2016-10-27 08:14:43 +0100

    Reinitalized as JSweet project

---
+++ .generated/bundle.ts
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Generated from Java with JSweet 1.2.0-SNAPSHOT - http://www.jsweet.org */ namespace fuzztest {     
+
+      
+
+
+
+
+
+/**
+ * @author peter
+ */
+export class TestMain {         
+
+
+
+
+
+/**
+ * @param args
+ */
+public static main(args : string[]) {             
+
+console.log("Hello World");         }     }     TestMain["__classname"] = "fuzztest.TestMain";  } 
+
+fuzztest.TestMain.main(null);
